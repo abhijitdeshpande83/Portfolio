1148889b6662177f34fc47e0253da8ebd5869ba8
Abhijit Deshpande <abhijitdeshpande83@gmail.com>
2026-05-21 22:08:00 -0500

    fix(intelliqa): correct variable mismatch and standardize documents parameter

--- NLPGenHub/NLPGenHub_urls.py
+++ NLPGenHub/NLPGenHub_urls.py
@@ -10,21 +10,5 @@ urlpatterns = [
     path('atlas/',atlas, name='atlas'),
     path('atlas_agent/',lambda_proxy,name='atlas_agent'),   
     path('aura/',aura, name='aura'),
-<<<<<<< HEAD
-<<<<<<< HEAD
     path('aura_agent/<str:session_id>/',aura_agent,name='aura_agent')
-=======
-    path('aura_agent/',aura_agent,name='aura_agent')
-<<<<<<< HEAD
-=======
-    path('taskpilot/',taskpilot, name='taskpilot'),
-    path('agent/',lambda_proxy,name='agent'),   
-    path('aura/',aura, name='aura')
->>>>>>> 57a0f659 (feat(AURA): add UI to AURA project)
->>>>>>> afeb74f4 (chore(aura): resolve rebase conflicts)
-=======
->>>>>>> 7624b4b6 (chore(aura): resolve rebase conflicts)
-=======
-    path('aura_agent/<str:session_id>/',aura_agent,name='aura_agent')
->>>>>>> c9a42532 (chore(aura): resolve rebase conflicts)
 ]
